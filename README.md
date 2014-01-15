86745113abb2c0b4f5ebdbc844d2f8095f62f6d9
Jeremy Ward <jrmy.ward@gmail.com>
2014-01-15 09:28:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ocd-rackbox-cookbook cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ocd-rackbox-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ocd-rackbox-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+

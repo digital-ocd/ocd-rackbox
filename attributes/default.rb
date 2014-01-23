5534f4e306cb44656ad07a6541e5662f20ceee95
Jeremy Ward <jrmy.ward@gmail.com>
2014-01-23 13:48:45 -0800

    Updated default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,26 +1,17 @@
-node.default['ocd_rackbox'] = {
-  home_dir: "/home/ocd",
-  group: 'sudo',
-  password: "$1$3XJlGhET$Zz2s1s0yURmac7p1u1Peh.",
-  no_password_cmds: [
-    "/etc/init.d/nginx"
-  ],
-  ssh_keys: [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL4enOfTs8LI0KjeGIDQwv0Urstk5sjMjdv8w8jAnWeI4DQ4+RXPjC77F/SrSuLUwUbgO2jAGl6VhgmQ6urnbI7mgZebSMMCobo6qpsvkYW4yAoXoJl1vWZgLbusfl9UqZtRYTFxu3y8z1pZQY69yjRT8AI57AS3XeGG4BpstEpmwPhhhPPQoiBMF6k6rCTh0sivHC0/60uJa6TuKUGXgt4Df97PqXe/Q4ROd8Oo7SZDprqajs43TjrKzKA6ALuurwX7FVi5ZQO4GPRIYi/m5QX7xgvvB5w4YLnOUKr6lFsHOSbjg1ztNqPxSxonJhq0ppqHT3dFNngfN274dztC+t jrmy.ward@gmail.com"
-  ],
-  user: 'ocd'
-}
+node.default['ocd_rackbox']['user']             = 'ocd'
+node.default['ocd_rackbox']['db_root_password'] = 'MyRandomdBpassword'
+node.default['ocd_rackbox']['home_dir']         = "/home/#{node['ocd_rackbox']['user']}"
+node.default['ocd_rackbox']['group']            = 'sudo'
+node.default['ocd_rackbox']['password']         = "$1$3XJlGhET$Zz2s1s0yURmac7p1u1Peh."
+node.default['ocd_rackbox']['no_password_cmds'] = [
+  "/etc/init.d/nginx"
+]
+node.default['ocd_rackbox']['ssh_keys']         = [
+  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL4enOfTs8LI0KjeGIDQwv0Urstk5sjMjdv8w8jAnWeI4DQ4+RXPjC77F/SrSuLUwUbgO2jAGl6VhgmQ6urnbI7mgZebSMMCobo6qpsvkYW4yAoXoJl1vWZgLbusfl9UqZtRYTFxu3y8z1pZQY69yjRT8AI57AS3XeGG4BpstEpmwPhhhPPQoiBMF6k6rCTh0sivHC0/60uJa6TuKUGXgt4Df97PqXe/Q4ROd8Oo7SZDprqajs43TjrKzKA6ALuurwX7FVi5ZQO4GPRIYi/m5QX7xgvvB5w4YLnOUKr6lFsHOSbjg1ztNqPxSxonJhq0ppqHT3dFNngfN274dztC+t jrmy.ward@gmail.com"
+]
 
 node.set['postgresql']['apt_distribution']  = 'precise'
 node.set['postgresql']['version']           = "9.2"
-# node.set["ocd_postgres"]["users"] << {
-#   "username": "postgres",
-#   "password": "$1$3XJlGhET$Zz2s1s0yURmac7p1u1Peh.",
-#   "superuser": true,
-#   "createdb": false,
-#   "login": true
-# }
-
 node.default['ocd_rackbox']['ruby'] = {
   versions: %w(2.0.0-p247),
   global_version: "2.0.0-p247"

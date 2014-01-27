5de69b6f2f8b83852f74667f18928b8d3c5b8193
Jeremy Ward <jrmy.ward@gmail.com>
2014-01-26 21:19:33 -0800

    Postgres fixes.

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -3,11 +3,6 @@
 # Recipe:: postgres
 #
 
-root_password = node["ocd_rackbox"]["db_root_password"]
-if root_password
-  Chef::Log.info %(Set node["postgresql"]["users"]["password"] attributes to node["ocd_rackbox"]["db_root_password"])
-  node.set["postgresql"]["users"]["password"] = root_password
-end
-
 include_recipe 'postgresql::client'
 include_recipe 'postgresql::server'
+include_recipe 'postgresql::server_dev'
